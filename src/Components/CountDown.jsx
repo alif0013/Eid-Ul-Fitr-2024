57ee48fd83e17583e4b1b400dcfdb08297fd61b0
alif0013 <mdalifh36@gmail.com>
2024-04-10 00:57:30 +0600

    host surge

--- src/Components/CountDown.jsx
+++ src/Components/CountDown.jsx
@@ -3,7 +3,7 @@ import Celebration from "./Celebration";
 
 const CountdownTimer = () => {
   const calculateTimeLeft = () => {
-    const difference = +new Date() - +new Date();
+    const difference = +new Date("2024-04-11") - +new Date();
     let timeLeft = {};
 
     if (difference > 0) {
@@ -45,14 +45,17 @@ const CountdownTimer = () => {
   return (
     <div className="flex justify-center my-5">
       {timerComponents.length ? (
-        <h1 className="md:text-xl border-purple-600 border-[1px] font-medium text-green-400 p-4 rounded-md hover:border-blue-600"> {timerComponents} </h1>
+        <>
+          <h1 className="md:text-xl border-purple-600 border-[1px] font-medium text-green-400 p-4 rounded-md hover:border-blue-600"> {timerComponents} </h1>
+
+        </>
       ) : (
 
         <>
           <Celebration></Celebration>
-          <h1 className="text-2xl md:text-5xl rounded-md drop-shadow-md bg-black p-4 text-green-400 border-purple-600 border-[1px]  font-semibold  hover:text-blue-500 hover:drop-shadow-lg">EId Mubarak !</h1>
+          <h1 className="text-2xl md:text-5xl rounded-md drop-shadow-md bg-black p-4 text-green-400 border-purple-600 border-[1px]  font-semibold  hover:text-blue-600 hover:drop-shadow-lg">EId Mubarak !</h1>
 
-          
+
         </>
       )}
     </div>
