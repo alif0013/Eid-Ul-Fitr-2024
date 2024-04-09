6d5ae83f3116310455fe769fe1c7dbd2cac368d4
alif0013 <mdalifh36@gmail.com>
2024-04-10 00:18:33 +0600

    added neew design

--- src/Components/CountDown.jsx
+++ src/Components/CountDown.jsx
@@ -3,15 +3,15 @@ import Celebration from "./Celebration";
 
 const CountdownTimer = () => {
   const calculateTimeLeft = () => {
-    const difference = +new Date("2024-04-8") - +new Date();
+    const difference = +new Date("2024-04-9") - +new Date();
     let timeLeft = {};
 
     if (difference > 0) {
       timeLeft = {
         days: Math.floor(difference / (1000 * 60 * 60 * 24)),
         hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
+        min: Math.floor((difference / 1000 / 60) % 60),
+        sec: Math.floor((difference / 1000) % 60),
       };
     }
 
@@ -45,12 +45,12 @@ const CountdownTimer = () => {
   return (
     <div className="flex justify-center my-5">
       {timerComponents.length ? (
-        <h1 className="md:text-xl font-medium text-green-400 p-4 bg-black rounded"> {timerComponents} </h1>
+        <h1 className="md:text-xl border-purple-600 border-[1px] font-medium text-green-400 p-4 rounded-md hover:border-blue-600"> {timerComponents} </h1>
       ) : (
 
         <>
           <Celebration></Celebration>
-          <span className="text-2xl md:text-5xl rounded-md drop-shadow-md bg-black p-4 text-green-500 font-mono font-semibold hover:bg-rose-500 hover:text-white hover:drop-shadow-lg">EId Mubarak!</span>
+          <h1 className="text-2xl md:text-5xl rounded-md drop-shadow-md bg-black p-4 text-green-400 border-purple-600 border-[1px]  font-semibold hover:bg-purple-600 hover:text-white hover:drop-shadow-lg">EId Mubarak !</h1>
 
         </>
       )}
